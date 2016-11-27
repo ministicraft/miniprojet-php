180a25dfcbb4f72204ee38b74f182fb54fd6bc39
ministicraft <worldtocraft@gmail.com>
2016-11-27 19:49:48 +0100

    ajout modal edit et delette + blockage bouton si aucune ligne selectionner

--- views/promos.php
+++ views/promos.php
@@ -3,8 +3,8 @@ include 'header.php';
 ?>
     <main>
         <a id="add" class="waves-effect waves-light btn" href="#modal1">Add</a>
-        <a id="delete" class="waves-effect waves-light btn"><i class="material-icons left">remove</i>Remove</a>
-        <a id="edit" class="waves-effect waves-light btn"><i class="material-icons left">edit</i>Edit</a>
+        <a id="delete" class="waves-effect waves-light btn disabled" href="#modal2"><i class="material-icons left">remove</i>Remove</a>
+        <a id="edit" class="waves-effect waves-light btn disabled" href="#modal3"><i class="material-icons left">edit</i>Edit</a>
         <div class="container">
             <table id="documents" class="striped"></table>
         </div>
@@ -13,17 +13,94 @@ include 'header.php';
                 <h4>Add a promo</h4>
                 <form>
                     <div class="input-field col s12">
-                        <select id="type">
-                            <option value="" disabled selected>Choisisser un type</option>
+                        <select id="select_cycle">
+                            <option value="" disabled selected>Choisisser un Cycle</option>
+                            <option value="1">CIR</option>
+                            <option value="2">CSI</option>
+                            <option value="3">M</option>
+                            <option id="other" value="other">Autre</option>
+                        </select>
+                        <label>Cycle</label>
+                    </div>
+                    <div id="other_cycle" class="input-field col s6" style="display: none">
+                        <input placeholder="Autre cycle" id="cycle" type="text" class="validate">
+                        <label for="cycle">Nouveau Cylce</label>
+                    </div>
+                    <div class="input-field col s12">
+                        <select id="year">
+                            <option value="" disabled selected>Choisisser une année</option>
                             <option value="1">Option 1</option>
                             <option value="2">Option 2</option>
-                            <option id="other" value="3">Autre</option>
                         </select>
-                        <label>Type de promo</label>
+                        <label>Année</label>
                     </div>
-                    <div id="other_type" class="input-field col s6" style="display: none">
-                        <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                        <label for="first_name">First Name</label>
+                    <div class="input-field col s12">
+                        <select id="select_loc">
+                            <option value="" disabled selected>Localisation</option>
+                            <option value="1">Brest</option>
+                            <option value="2">Renne</option>
+                            <option id="other" value="other">Autre</option>
+                        </select>
+                        <label>Localisation</label>
+                    </div>
+                    <div id="other_loc" class="input-field col s6" style="display: none">
+                        <input placeholder="Nouvelle Ville" id="loc" type="text" class="validate">
+                        <label for="loc">Nouvelle Localisation</label>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+            </div>
+        </div>
+        <div id="modal2" class="modal">
+            <div class="modal-content">
+                <p>Ëtes vous sure de vouloir supprimer cette promo ?</p>
+
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Disagree</a>
+            </div>
+        </div>
+        <div id="modal3" class="modal">
+            <div class="modal-content">
+                <h4>Add a promo</h4>
+                <form>
+                    <div class="input-field col s12">
+                        <select id="select_cycle">
+                            <option value="" disabled selected>Choisisser un Cycle</option>
+                            <option value="1">CIR</option>
+                            <option value="2">CSI</option>
+                            <option value="3">M</option>
+                            <option id="other" value="other">Autre</option>
+                        </select>
+                        <label>Cycle</label>
+                    </div>
+                    <div id="other_cycle" class="input-field col s6" style="display: none">
+                        <input placeholder="Autre cycle" id="cycle" type="text" class="validate">
+                        <label for="cycle">Nouveau Cylce</label>
+                    </div>
+                    <div class="input-field col s12">
+                        <select id="year">
+                            <option value="" disabled selected>Choisisser une année</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                        </select>
+                        <label>Année</label>
+                    </div>
+                    <div class="input-field col s12">
+                        <select id="select_loc">
+                            <option value="" disabled selected>Localisation</option>
+                            <option value="1">Brest</option>
+                            <option value="2">Renne</option>
+                            <option id="other" value="other">Autre</option>
+                        </select>
+                        <label>Localisation</label>
+                    </div>
+                    <div id="other_loc" class="input-field col s6" style="display: none">
+                        <input placeholder="Nouvelle Ville" id="loc" type="text" class="validate">
+                        <label for="loc">Nouvelle Localisation</label>
                     </div>
                 </form>
             </div>
@@ -35,12 +112,20 @@ include 'header.php';
     <script>
         $(document).ready(function() {
             //perso
-            $('#type').on('change',function(){
-                if( $(this).val()==="3"){
-                    $("#other_type").show()
+            $('#select_cycle').on('change',function(){
+                if( $(this).val()==="other"){
+                    $("#other_cycle").show()
                 }
                 else{
-                    $("#other_type").hide()
+                    $("#other_cycle").hide()
+                }
+            });
+            $('#select_loc').on('change',function(){
+                if( $(this).val()==="other"){
+                    $("#other_loc").show()
+                }
+                else{
+                    $("#other_loc").hide()
                 }
             });
 
@@ -78,10 +163,14 @@ include 'header.php';
             $('#documents tbody').on( 'click', 'tr', function () {
                 if ( $(this).hasClass('selected') ) {
                     $(this).removeClass('selected');
+                    $('#delete').addClass('disabled');
+                    $('#edit').addClass('disabled');
                 }
                 else {
                     table.$('tr.selected').removeClass('selected');
                     $(this).addClass('selected');
+                    $('#delete').removeClass('disabled');
+                    $('#edit').removeClass('disabled');
                 }
             } );
             $('#add').click( function () {
