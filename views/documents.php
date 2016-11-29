4a2bf60566544d0b5fbd688008a4d10d3125775b
ministicraft <worldtocraft@gmail.com>
2016-11-29 11:34:20 +0100

    dans le form ajout doc, récupère les promo depuis json

--- views/documents.php
+++ views/documents.php
@@ -9,42 +9,45 @@ include 'header.php';
             <table id="documents" class="striped"></table>
         </div>
         <div id="modal1" class="modal">
-            <div class="modal-content">
-                <h4>Add a document</h4>
-                <form>
-                    <div class="input-field col s12">
-                        <select id="select_cycle">
-                            <option value="" disabled selected>Choisissez un cycle</option>
-                            <option id="other" value="other">Autre</option>
-                        </select>
-                        <label>Cycle</label>
-                    </div>
-                    <div id="other_cycle" class="input-field col s6" style="display: none">
-                        <input placeholder="Autre cycle" id="cycle" type="text" class="validate">
-                        <label for="cycle">Nouveau Cylce</label>
-                    </div>
-                    <div class="input-field col s12">
-                        <select id="select_annee">
-                            <option value="" disabled selected>Choisisser une année</option>
-                        </select>
-                        <label>Année</label>
-                    </div>
-                    <div class="input-field col s12">
-                        <select id="select_loc">
-                            <option value="" disabled selected>Localisation</option>
-                            <option id="other" value="other">Autre</option>
-                        </select>
-                        <label>Localisation</label>
-                    </div>
-                    <div id="other_loc" class="input-field col s6" style="display: none">
-                        <input placeholder="Nouvelle Ville" id="loc" type="text" class="validate">
-                        <label for="loc">Nouvelle Localisation</label>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-            </div>
+            <form>
+                <div class="modal-content">
+                    <h4>Add a document</h4>
+                        <div class="input-field col s12">
+                            <select id="select_promo">
+                                <option value="" disabled selected>choisissez une promo</option>
+                                <option id="other" value="other">Autre</option>
+                            </select>
+                            <label>Promo</label>
+                        </div>
+                        <div id="other_cycle" class="input-field col s6" style="display: none">
+                            <input placeholder="Autre cycle" id="cycle" type="text" class="validate">
+                            <label for="cycle">Rang</label>
+                        </div>
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>File</span>
+                                <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text">
+                            </div>
+                        </div>
+                        <div class="input-field col s12">
+                            <select id="select_loc">
+                                <option value="" disabled selected>Localisation</option>
+                                <option id="other" value="other">Autre</option>
+                            </select>
+                            <label>Localisation</label>
+                        </div>
+                        <div id="other_loc" class="input-field col s6" style="display: none">
+                            <input placeholder="Nouvelle Ville" id="loc" type="text" class="validate">
+                            <label for="loc">Nouvelle Localisation</label>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+            </form>
         </div>
         <div id="modal2" class="modal">
             <div class="modal-content">
@@ -61,18 +64,18 @@ include 'header.php';
                 <h4>Edit a document</h4>
                 <form>
                     <div class="input-field col s12">
+                        <select id="edit_select_promo">
+                            <option value="" disabled selected>Choisissez une promo</option>
+                        </select>
+                        <label>Promo</label>
+                    </div>
+                    <div class="input-field col s12">
                         <input type="text" id="input_rank" class="autocomplete">
                         <label for="autocomplete-input">Rang</label>
                     </div>
                     <div id="other_rank" class="input-field col s6" style="display: none">
                         <input placeholder="Autre rang" id="rank" type="text" class="validate">
                         <label for="rank">Nouveau rang</label>
-                    </div>
-                    <div class="input-field col s12">
-                        <select id="edit_select_promo">
-                            <option value="" disabled selected>Choisissez une promo</option>
-                        </select>
-                        <label>Promo</label>
                     </div>
                     <div class="input-field col s12">
 
@@ -92,14 +95,35 @@ include 'header.php';
     <script>
         $(document).ready(function() {
 
-            $.getJSON( "api/promo", function( data ) {
+            $.getJSON( "api/promos", function( data ) {
                 $.each( data, function( key, val ) {
-                    $("#select_promo").append("<option value="+val+">"+val+"</option>");
-                    $("#edit_select_promo").append("<option value="+val+">"+val+"</option>");
+                    console.log(key);
+                    console.log(val);
+                    cycle=val['cycle'];
+                    if(val['loc']!="N/A") {
+                        loc = "_" + val['loc'];
+                    } else {
+                        loc = "";
+                    }
+                    annee="_"+val['annee'];
+
+
+                    $("#select_promo").append("<option value="+val['id']+">"+cycle + loc +  annee +"</option>");
+                    $("#edit_select_promo").append("<option value="+val['id']+">"+val['cycle']+" " + val['loc'] + " " + val['annee'] +"</option>");
                 });
                 $(document).ready(function() {
                     $('select').material_select();
                 });
+            });
+
+            var fd = new FormData(document.querySelector("form"));
+            fd.append("CustomField", "This is some extra data");
+            $.ajax({
+                url: "stash.php",
+                type: "POST",
+                data: fd,
+                processData: false,  // tell jQuery not to process the data
+                contentType: false   // tell jQuery not to set contentType
             });
 
 
