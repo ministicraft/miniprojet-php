95ac6444bcc5345b223ba611141e481b718c6315
ministicraft <worldtocraft@gmail.com>
2016-12-31 16:53:50 +0100

    test

--- rentree/config.php
+++ rentree/config.php
@@ -14,13 +14,34 @@
   $DbUser = "cir32016";
   $DbPassword = "cir32016";
 
+$libellePromo = array (
+    "1&#x02B3;&#x1D49; année, Cycle Biologie Sciences et Technologies" => "CBIO_A1",
+    "1&#x02B3;&#x1D49; année, Cycle Sciences de l'Ingénieur" => "CSI_A1",
+    "1&#x02B3;&#x1D49; année, Cycle Informatique et Réseaux (Brest)" => "CIR_BREST_A1",
+    "1&#x02B3;&#x1D49; année, Cycle Informatique et Réseaux (Rennes)" => "CIR_RENNES_A1",
+    "1&#x02B3;&#x1D49; année, BTS Prépa" => "BTSPREPA_A1",
+    "2&#x1D49; année, Cycle Sciences de l'Ingénieur" => "CSI_A2",
+    "2&#x1D49; année, Cycle Informatique et Réseaux (Brest)" => "CIR_BREST_A2",
+    "2&#x1D49; année, Cycle Informatique et Réseaux (Rennes)" => "CIR_RENNES_A2",
+    "2&#x1D49; année, BTS Prépa" => "BTSPREPA_A2",
+    "3&#x1D49; année, Cycle Sciences de l'Ingénieur" => "CSI_A3",
+    "3&#x1D49; année, Cycle Informatique et Réseaux (alternant)" => "CIR_A3_ALT",
+    "3&#x1D49; année, Cycle Informatique et Réseaux (non alternant)" => "CIR_A3_NONALT",
+    "3&#x1D49; année, Cycle Ingénieur Par l'Apprentissage" => "CIPA_A3",
+    "4&#x1D49; année, Majeure - M1" => "M_A4",
+    "4&#x1D49; année, Cycle Ingénieur Par l'Apprentissage" => "CIPA_A4",
+    "5&#x1D49; année, Majeure - M2 (alternant)" => "M_A5_ALT",
+    "5&#x1D49; année, Majeure - M2 (non alternant)" => "M_A5_NONALT",
+    "5&#x1D49; année, Cycle Ingénieur Par l'Apprentissage" => "CIPA_A5"
+);
+
   $DbLink = mysql_connect($DbHost, $DbUser, $DbPassword) or die('erreur de connexion au serveur');
   mysql_select_db($DbName) or die('erreur de connexion a la base de donnees');
   mysql_query("SET NAMES 'utf8'");
   $query = "SELECT * FROM promo";
 
   $result = mysql_query($query);
-  $libellePromo = array();
+  //$libellePromo = array();
 
   while($data = mysql_fetch_array($result)) {
 
@@ -36,6 +57,7 @@
         $promo = $data['cycle'] . '_' . $data['annee'];
     }
     $libellePromo[$data['libelle']] = $promo;
+    print_r($libellePromo);
   }
 
   require_once("lib.php");
