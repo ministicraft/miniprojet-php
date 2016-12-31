86cf56ab888caf764ae55efccd0abebf7e4ecc4a
ministicraft <worldtocraft@gmail.com>
2016-12-31 16:25:19 +0100

    mise a jout fichier sql

--- db/doc_rentree.sql
+++ db/doc_rentree.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Client: localhost
--- Généré le: Sam 26 Novembre 2016 à 18:07
--- Version du serveur: 5.5.53-0ubuntu0.14.04.1
--- Version de PHP: 5.5.9-1ubuntu4.20
+-- Client :  db
+-- Généré le :  Sam 31 Décembre 2016 à 14:08
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,53 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données: `doc_rentree`
+-- Base de données :  `doc_rentree`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annee`
+--
+
+CREATE TABLE `annee` (
+  `annee` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `annee`
+--
+
+INSERT INTO `annee` (`annee`) VALUES
+('A1'),
+('A2'),
+('A3'),
+('A4'),
+('A5');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cycles`
+--
+
+CREATE TABLE `cycles` (
+  `cycle` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `cycles`
+--
+
+INSERT INTO `cycles` (`cycle`) VALUES
+('BTS'),
+('CBIO'),
+('CIR'),
+('CSI'),
+('M');
 
 -- --------------------------------------------------------
 
@@ -26,8 +68,8 @@ SET time_zone = "+00:00";
 -- Structure de la table `data`
 --
 
-CREATE TABLE IF NOT EXISTS `data` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL,
   `identifiant` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `nom_fils` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `prenom_fils` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -35,9 +77,8 @@ CREATE TABLE IF NOT EXISTS `data` (
   `tel_mobile` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `courriel` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `date` datetime NOT NULL,
-  `ip` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `ip` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `data`
@@ -52,21 +93,20 @@ INSERT INTO `data` (`id`, `identifiant`, `nom_fils`, `prenom_fils`, `ddn_fils`, 
 -- Structure de la table `document`
 --
 
-CREATE TABLE IF NOT EXISTS `document` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `document` (
+  `id` int(11) NOT NULL,
   `rang` int(11) NOT NULL,
   `promo` varchar(256) COLLATE utf8_bin NOT NULL,
   `libelle` varchar(256) COLLATE utf8_bin NOT NULL,
-  `fichier` varchar(256) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=73 ;
+  `fichier` varchar(256) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Contenu de la table `document`
 --
 
 INSERT INTO `document` (`id`, `rang`, `promo`, `libelle`, `fichier`) VALUES
-(1, 1, '', 'Dates des rentrées à l''ISEN', 'rentreesISENBrestRennes1617.pdf'),
+(1, 1, '', 'Dates des rentrées à l\'ISEN', 'rentreesISENBrestRennes1617.pdf'),
 (2, 1, 'CSI_A1', 'Calendrier Classes Préparatoires', 'A12/CSI1et2Calendriers1617.pdf'),
 (3, 1, 'CSI_A2', 'Calendrier Classes Préparatoires', 'A12/CSI1et2Calendriers1617.pdf'),
 (4, 1, 'CIR_BREST_A1', 'Calendrier CIR', 'A12/CIR1et2Calendriers1617.pdf'),
@@ -130,14 +170,38 @@ INSERT INTO `document` (`id`, `rang`, `promo`, `libelle`, `fichier`) VALUES
 (62, 5, 'CBIO_A1', 'Contrat de mise à disposition ordinateur portable', 'A12/contratMiseDisposition2016BIOST1.pdf'),
 (63, 5, 'CIR_BREST_A1', 'Contrat de mise à disposition ordinateur portable', 'A12/contratMiseDisposition2016CIR1.pdf'),
 (64, 5, 'CIR_RENNES_A1', 'Contrat de mise à disposition ordinateur portable', 'A12/contratMiseDisposition2016CIR1.pdf'),
-(65, 6, 'CBIO_A1', 'Note d''information assurance ordinateur portable', 'A12/NoteInformation2016-2017CIR.pdf'),
-(66, 6, 'CIR_BREST_A1', 'Note d''information assurance ordinateur portable', 'A12/NoteInformation2016-2017CIR.pdf'),
-(67, 6, 'CIR_RENNES_A1', 'Note d''information assurance ordinateur portable', 'A12/NoteInformation2016-2017CIR.pdf'),
+(65, 6, 'CBIO_A1', 'Note d\'information assurance ordinateur portable', 'A12/NoteInformation2016-2017CIR.pdf'),
+(66, 6, 'CIR_BREST_A1', 'Note d\'information assurance ordinateur portable', 'A12/NoteInformation2016-2017CIR.pdf'),
+(67, 6, 'CIR_RENNES_A1', 'Note d\'information assurance ordinateur portable', 'A12/NoteInformation2016-2017CIR.pdf'),
 (68, 7, 'CBIO_A1', 'Intégration - lettre aux premières années', 'A12/CSI1_CIR1_BIOST_integration_lettre_aux_premieres_annees.pdf'),
 (69, 6, 'CSI_A1', 'Intégration - lettre aux premières années', 'A12/CSI1_CIR1_BIOST_integration_lettre_aux_premieres_annees.pdf'),
 (70, 7, 'CIR_BREST_A1', 'Intégration - lettre aux premières années', 'A12/CSI1_CIR1_BIOST_integration_lettre_aux_premieres_annees.pdf'),
 (71, 7, 'CIR_RENNES_A1', 'Intégration - lettre aux premières années', 'A12/CSI1_CIR1_BIOST_integration_lettre_aux_premieres_annees.pdf'),
-(72, 2, '', 'Offre banque BNP', 'BNP_Flyer_Rentree_2016.pdf');
+(72, 2, '', 'Offre banque BNP', 'BNP_Flyer_Rentree_2016.pdf'),
+(77, 3, 'M_A1', 'photo2', 'A12/WIN_20161219_12_23_29_Pro.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `localisations`
+--
+
+CREATE TABLE `localisations` (
+  `localisation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `localisations`
+--
+
+INSERT INTO `localisations` (`localisation`) VALUES
+(''),
+('BREST'),
+('MORLAIX'),
+('N/A'),
+('NANTES'),
+('Petzouille les oies'),
+('RENNES');
 
 -- --------------------------------------------------------
 
@@ -145,30 +209,105 @@ INSERT INTO `document` (`id`, `rang`, `promo`, `libelle`, `fichier`) VALUES
 -- Structure de la table `promo`
 --
 
-CREATE TABLE IF NOT EXISTS `promo` (
-  `type` varchar(60) NOT NULL DEFAULT '',
-  `localisation` varchar(60) NOT NULL DEFAULT '',
-  `annee` varchar(60) NOT NULL,
-  `alias` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`type`,`localisation`,`annee`),
-  UNIQUE KEY `alias` (`alias`),
-  KEY `localisation` (`localisation`),
-  KEY `localisation_2` (`localisation`)
+CREATE TABLE `promo` (
+  `id` int(11) NOT NULL,
+  `cycle` varchar(255) NOT NULL,
+  `localisation` varchar(255) NOT NULL DEFAULT 'N/A',
+  `annee` varchar(255) NOT NULL,
+  `alt` tinyint(1) DEFAULT NULL,
+  `libelle` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `promo`
 --
 
-INSERT INTO `promo` (`type`, `localisation`, `annee`, `alias`) VALUES
-('CIR', '', 'A3', NULL),
-('CIR', 'BREST', 'A1', NULL),
-('CIR', 'BREST', 'A2', NULL),
-('CIR', 'RENNES', 'A1', NULL),
-('CIR', 'RENNES', 'A2', NULL),
-('CSI', '', 'A3', NULL),
-('CSI', 'BREST', 'A1', NULL),
-('CSI', 'BREST', 'A2', NULL);
+INSERT INTO `promo` (`id`, `cycle`, `localisation`, `annee`, `alt`, `libelle`) VALUES
+(12, 'CSI', 'RENNES', 'A1', NULL, 'Csi espace et'),
+(13, 'M', 'N/A', 'A1', NULL, 'toto'),
+(15, 'CIR', 'N/A', 'A3', NULL, 'test'),
+(17, 'BTS', 'BREST', 'A2', 1, 'test'),
+(18, 'CIR', 'MORLAIX', 'A1', 0, 'test'),
+(19, 'CSI', 'NANTES', 'A3', 0, 'tutu'),
+(21, 'BTS', 'BREST', 'A1', NULL, 'tutu'),
+(24, 'M', '', 'A4', NULL, 'titi'),
+(25, 'CIR', 'BREST', 'A1', NULL, 'CIR BREST A1');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `annee`
+--
+ALTER TABLE `annee`
+  ADD PRIMARY KEY (`annee`);
+
+--
+-- Index pour la table `cycles`
+--
+ALTER TABLE `cycles`
+  ADD PRIMARY KEY (`cycle`);
+
+--
+-- Index pour la table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `document`
+--
+ALTER TABLE `document`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `localisations`
+--
+ALTER TABLE `localisations`
+  ADD PRIMARY KEY (`localisation`);
+
+--
+-- Index pour la table `promo`
+--
+ALTER TABLE `promo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `localisation` (`localisation`),
+  ADD KEY `localisation_2` (`localisation`),
+  ADD KEY `cycle` (`cycle`),
+  ADD KEY `cycle_2` (`cycle`),
+  ADD KEY `year` (`annee`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `document`
+--
+ALTER TABLE `document`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+--
+-- AUTO_INCREMENT pour la table `promo`
+--
+ALTER TABLE `promo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `promo`
+--
+ALTER TABLE `promo`
+  ADD CONSTRAINT `promo_ibfk_1` FOREIGN KEY (`cycle`) REFERENCES `cycles` (`cycle`),
+  ADD CONSTRAINT `promo_ibfk_2` FOREIGN KEY (`localisation`) REFERENCES `localisations` (`localisation`),
+  ADD CONSTRAINT `promo_ibfk_3` FOREIGN KEY (`annee`) REFERENCES `annee` (`annee`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
